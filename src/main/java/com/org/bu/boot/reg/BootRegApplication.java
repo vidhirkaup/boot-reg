51cb78bf9f1ab2e094081d7ada20cd684afc6dca
vidhirkaup <vidhir.kaup@gmail.com>
2022-12-11 20:05:04 -0500

    initial commit

---
+++ src/main/java/com/org/bu/boot/reg/BootRegApplication.java
@@ -0,0 +1,13 @@
+package com.org.bu.boot.reg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/bu/boot/reg/BootRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.bu.boot.reg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
